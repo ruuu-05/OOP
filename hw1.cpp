95fab4dffa85623061cb9ea9e2ae145801228b0c
Roaa Atif <roaaatif16@gmail.com>
2025-09-18 01:40:01 +0300

    hw 1

---
+++ hw1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+struct Date{
+ int month;
+ int year;
+ int day;
+ Date fromToday();
+};
+//////////////////////////////////
+//Unix time vs 3 int for h m s 
+
+int main(){
+    return 0;
+}
